8c4bca79cd947e9a52c1ec3150457f76607ef5f8
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-10-04 21:43:56 +0200

    refactoring variable names

--- src/com/tmbs/ta06_12/Main_EJ12.java
+++ src/com/tmbs/ta06_12/Main_EJ12.java
@@ -2,35 +2,34 @@ package com.tmbs.ta06_12;
 
 import javax.swing.JOptionPane;
 
-/*  Crea un array de números de un tamaño pasado por teclado, el array contendrá
-números aleatorios entre 1 y 300 y mostrará aquellos números que acaben en un dígito que
+/*  Crea un array de nï¿½meros de un tamaï¿½o pasado por teclado, el array contendrï¿½
+nï¿½meros aleatorios entre 1 y 300 y mostrarï¿½ aquellos nï¿½meros que acaben en un dï¿½gito que
 nosotros le indiquemos por teclado (debes controlar que se introduce un numero correcto),
 estos deben guardarse en un nuevo array.
 
-Por ejemplo, en un array de 10 posiciones le indicamos mostrar los números acabados en 5,
-podría salir 155, 25, etc.*/
+Por ejemplo, en un array de 10 posiciones le indicamos mostrar los nï¿½meros acabados en 5,
+podrï¿½a salir 155, 25, etc.*/
 public class Main_EJ12 {
 
 	public static void main(String[] args) {
 
-		int l = 0;
+		int sizeArray = 0;
 
 		// Pide longitud del array al usuario y comprueba que sea correcta
 		try {
 
 			do {
-				String lstring = JOptionPane.showInputDialog("Introduce la longitud del array a generar");
-				l = Integer.parseInt(lstring);
+				String inputSize = JOptionPane.showInputDialog("Introduce la longitud del array a generar");
+				sizeArray = Integer.parseInt(inputSize);
 
-				if (l < 1) {
-					JOptionPane.showMessageDialog(null,
-							"Por favor, introduce un numero positivo mayor de 0 y sin decimales.");
+				if (sizeArray < 1) {
+					JOptionPane.showMessageDialog(null,"Por favor, introduce un nï¿½mero entero porsitivo");
 				}
 
-			} while (l < 1);
+			} while (sizeArray < 1);
 
 			// Crea el array con la longitud especificada
-			int[] array = new int[l];
+			int[] array = new int[sizeArray];
 
 			// Rellena el array con numeros del 1 al 300
 			array = fillArray(array);
@@ -39,7 +38,7 @@ public class Main_EJ12 {
 			showArray(array);
 
 		} catch (Exception e) {
-			System.out.println("El dato introducico no es correcto");
+			System.out.println("El dato introducido no es correcto");
 		}
 
 	}
@@ -48,21 +47,23 @@ public class Main_EJ12 {
 	public static int[] fillArray(int[] array) {
 		int min = 1;
 		int max = 300;
-		int rango = max - min;
-
+		int range = max - min;
+		
+		
+		
 		for (int i = 0; i < array.length; i++) {
-			array[i] = (int) ((Math.random() * (rango + 1)) + min);
+			array[i] = (int) ((Math.random() * (range + 1)) + min);
 		}
 		return array;
 	}
 
-	// Funcion para mostrar contenidos del array finalizados en el numero determinado
+	// Metodo para mostrar contenidos del array finalizados en el numero determinado
 	public static void showArray(int[] array) {
 
 		int num = 10;
 		do {
-			String numstring = JOptionPane.showInputDialog("Introduce el digito final");
-			num = Integer.parseInt(numstring);
+			String numStr = JOptionPane.showInputDialog("Introduce el digito final");
+			num = Integer.parseInt(numStr);
 			if (num > 9 || num < 0) {
 				JOptionPane.showMessageDialog(null, "Error, el numero tiene que ser entre 0 y 9");
 			}
